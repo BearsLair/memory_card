7dfc75ff69b6df004dcf01542f13199a11b8c003
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-18 15:16:10 -0700

    basic card for testing

---
+++ memory_card/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div className="card-div">
+      <img className="card-img" src="images/example.png"></img>
+      <p className="card-name">Charmander</p>
+    </div>
+  );
+}
+
+export default Card;
