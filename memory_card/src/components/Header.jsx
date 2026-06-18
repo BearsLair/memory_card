a6c18a465c70f661c407d2560f1adcc1519fe621
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-18 15:16:30 -0700

    basic content added

---
+++ memory_card/src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <div>
+      <div>
+        <h1>Memory Game</h1>
+        <p>
+          Click an image to gain points. But not the same image more than once!
+        </p>
+      </div>
+      <div>
+        <p>Current Score: </p>
+        <p>Best Score: </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
