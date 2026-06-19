aa75ee30860f37d94c6c11f0779d541784438b1b
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-18 22:49:42 -0700

    can now fetch list and add data to state

--- memory_card/src/App.jsx
+++ memory_card/src/App.jsx
@@ -4,10 +4,45 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [fetchedData, setFetchedData] = useState([]);
+  const [fetchedPokemon, setFetchedPokemon] = useState([]);
   const [randomizedData, setRandomizedData] = useState([]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    let pokemonList = [];
+
+    let pokemon = {};
+
+    try {
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=12")
+        .then((res) => res.json())
+        .then(function (firstPokemon) {
+          firstPokemon.results.forEach(function (pokemon) {
+            fetchPokemonData(pokemon);
+          });
+        });
+    } catch (error) {
+      console.log(error);
+    }
+
+    function fetchPokemonData(pokemon) {
+      let url = pokemon.url;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then(function (data) {
+          console.log(data.id);
+          pokemon.id = data.id;
+          console.log(data.name);
+          pokemon.name = data.name;
+          console.log(data.sprites.other["official-artwork"].front_default);
+          pokemon.image = data.sprites.other["official-artwork"].front_default;
+
+          pokemonList.push(pokemon);
+        });
+
+      setFetchedPokemon(pokemonList);
+    }
+  }, []);
 
   return (
     <>
