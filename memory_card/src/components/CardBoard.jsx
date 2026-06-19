f552f2090694994e95f59c016efa24ada13a0326
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-18 17:51:53 -0700

    added cards for general styling

---
+++ memory_card/src/components/CardBoard.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card";
+
+function CardBoard() {
+  return (
+    <div className="gameboard-grid">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default CardBoard;
