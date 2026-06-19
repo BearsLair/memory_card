b2e1acca9f1e00858fdcb295b1e77091757404a9
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-18 20:32:28 -0700

    useState and useEffect set up

--- memory_card/src/App.jsx
+++ memory_card/src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "./components/Header";
 import CardBoard from "./components/CardBoard";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+  const [randomizedData, setRandomizedData] = useState([]);
+
+  useEffect(() => {});
+
   return (
     <>
       <Header />
